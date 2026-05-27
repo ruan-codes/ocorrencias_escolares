5aeea60b23c30aa00aa2737046909379de9e6cde
ruan-codes <ruan030808@gmail.com>
2026-05-26 23:40:57 -0300

    feat: register Ocorrencia model in admin with custom display and filtering options

--- ocorrencias_project/ocorrencias/admin.py
+++ ocorrencias_project/ocorrencias/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Ocorrencia
 
-# Register your models here.
+@admin.register(Ocorrencia)
+class OcorrenciaAdmin(admin.ModelAdmin):
+    list_display = ["nome_aluno", "curso", "ano", "gravidade", "data", "criado_em"]
+    list_filter = ["curso", "ano", "gravidade"]
+    search_fields = ["nome_aluno"]
+    ordering = ["-criado_em"]
